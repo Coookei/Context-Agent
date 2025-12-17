7a9c1f253de74996d53309225b8df2004a681235
Coookei <89310401+Coookei@users.noreply.github.com>
2025-12-17 14:50:06 +0000

    feat(server): add environment variable management

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import type { Request, Response } from "express";
 
@@ -5,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
