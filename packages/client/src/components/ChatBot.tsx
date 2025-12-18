36d98e4861f02b352b4f7eeb448035a6a64ddf3d
Coookei <89310401+Coookei@users.noreply.github.com>
2025-12-18 17:47:32 +0000

    feat(client): add chat bot form using react-hook-form

---
+++ packages/client/src/components/ChatBot.tsx
@@ -0,0 +1,50 @@
+import type { KeyboardEvent } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaArrowUp } from 'react-icons/fa';
+import { Button } from './ui/button';
+
+type FormData = {
+  prompt: string;
+};
+
+const ChatBot = () => {
+  const { register, handleSubmit, reset, formState } = useForm<FormData>({
+    mode: 'all',
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+    reset();
+  };
+
+  const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      handleSubmit(onSubmit)();
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      onKeyDown={onKeyDown}
+      className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+    >
+      <textarea
+        {...register('prompt', {
+          required: true,
+          setValueAs: (v) => v.trim(),
+          validate: (v) => v.length > 0,
+        })}
+        className="w-full border-0 focus:outline-0 resize-none"
+        placeholder="Ask anything"
+        maxLength={1000}
+      />
+      <Button disabled={!formState.isValid} className="rounded-full w-9 h-9">
+        <FaArrowUp />
+      </Button>
+    </form>
+  );
+};
+
+export default ChatBot;
