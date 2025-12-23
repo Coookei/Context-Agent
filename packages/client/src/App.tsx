587def06ea2411447fb63db5d3ea40e6872761d4
Coookei <89310401+Coookei@users.noreply.github.com>
2025-12-23 10:32:21 +0000

    chore: rename project and related components

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,9 @@
-import ChatBot from './components/chat/ChatBot';
+import ChatAgent from './components/chat/ChatAgent';
 
 function App() {
   return (
     <div className="p-4 h-screen w-full">
-      <ChatBot />
+      <ChatAgent />
     </div>
   );
 }
--- packages/client/src/components/chat/ChatAgent.tsx
+++ packages/client/src/components/chat/ChatAgent.tsx
@@ -8,28 +8,31 @@ type ChatResponse = {
   message: string;
 };
 
-const ChatBot = () => {
+const ChatAgent = () => {
   const [messages, setMessages] = useState<Message[]>([]);
-  const [isBotTyping, setIsBotTyping] = useState(false);
+  const [isAgentTyping, setIsAgentTyping] = useState(false);
   const [error, setError] = useState<string>('');
   const conversationId = useRef(crypto.randomUUID());
 
   const onSubmit = async ({ prompt }: ChatFormData) => {
     try {
       setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
-      setIsBotTyping(true);
+      setIsAgentTyping(true);
       setError('');
 
       const { data } = await axios.post<ChatResponse>('/api/chat', {
         prompt,
         conversationId: conversationId.current,
       });
-      setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
+      setMessages((prev) => [
+        ...prev,
+        { content: data.message, role: 'agent' },
+      ]);
     } catch (error) {
       console.error(error);
       setError('Something went wrong. Please try again.');
     } finally {
-      setIsBotTyping(false);
+      setIsAgentTyping(false);
     }
   };
 
@@ -37,7 +40,7 @@ const ChatBot = () => {
     <div className="flex flex-col h-full">
       <div className="flex flex-col flex-1 gap-3 mb-10 overflow-y-auto">
         <ChatMessages messages={messages} />
-        {isBotTyping && <TypingIndicator />}
+        {isAgentTyping && <TypingIndicator />}
         {error && <p className="text-red-500">{error}</p>}
       </div>
       <ChatInput onSubmit={onSubmit} />
@@ -45,4 +48,4 @@ const ChatBot = () => {
   );
 };
 
-export default ChatBot;
+export default ChatAgent;
